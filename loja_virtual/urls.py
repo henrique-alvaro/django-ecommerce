4fbb0daa4c3ca8ec0a1d730e5b47e8b910ba380a
Henrique <henrique.coolcat15@gmail.com>
2023-11-02 18:27:29 -0300

    put imports and insert urlpatterns the about the about_page ans contact_page in loja_virtual/urls.py ans update .gitignore

--- loja_virtual/urls.py
+++ loja_virtual/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_page
+from .views import home_page, about_page, contract_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', home_page),
+    path('about/', home_page),
+    path('contaxt', home_page),
+    path('admin/', admin.site.urls),
 ]
+
