dce4d763c263f351d1a25e34db9d5e43ea2e9e51
Henrique <henrique.coolcat15@gmail.com>
2023-11-05 14:12:42 -0300

    template/about_page upgrade

--- loja_virtual/views.py
+++ loja_virtual/views.py
@@ -3,4 +3,24 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home_page.html')
+    context = {
+        'title': 'Página Principal',
+        'content': 'Bem-vindo a página principal',
+    }
+    return render(request, 'home_page.html', context)
+
+
+def about_page(request):
+    context = {
+        'title': 'Pagina Sobre',
+        'content': 'Bem-vindo a página sobre',
+    }
+    return render(request, '', context)
+
+
+def contact_page(request):
+    context = {
+        'title': 'Página de contato',
+        'content': 'Bem vindo a página de contato',
+    }
+    return render(request, '', context)
