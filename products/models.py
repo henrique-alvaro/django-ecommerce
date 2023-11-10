75d1bdaed13e3992f617e28e10085f2df78a7298
Henrique <henrique.coolcat15@gmail.com>
2023-11-10 14:23:17 -0300

    create products app

---
+++ products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+
+    def __str__(self):
+        return self.title
