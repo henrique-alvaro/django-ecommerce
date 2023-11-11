a0e554726eae4d2ebac1318776aedc7bf37392a0
Henrique <henrique.coolcat15@gmail.com>
2023-11-11 14:33:59 -0300

    create products/view detail and templates/products/detail.html and update loja_virtual/urls.py

--- loja_virtual/urls.py
+++ loja_virtual/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import home_page, about_page, contact_page, login_page, register_page
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 
 urlpatterns = [
     path('', home_page),
@@ -14,7 +14,8 @@ urlpatterns = [
     path('register/', register_page),
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),
-
+    path('products/<int:pk>', ProductDetailView.as_view()),
+    path('products-fbv/<int:pk>', product_detail_view),
 ]
 
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -19,3 +19,23 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/list.html", context)
+
+# Classe Detalhes
+class ProductDetailView(DetailView):
+    #traz todos os produtos do banco de dados sem filtrar nada
+    queryset = Product.objects.all()
+    template_name = 'products/detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(args)
+        return context
+
+# Function Detail
+def product_detail_view(request, pk=None, *args, **kwargs):
+    #instance = Product.objects.get(pk=pk)
+    instance = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': instance,
+    }
+    return render(request, 'products/detail.html', context)
