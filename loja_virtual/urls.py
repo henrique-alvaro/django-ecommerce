c286b0fedfae78f6260d54218e7f5fca986df4cf
Henrique <henrique.coolcat15@gmail.com>
2023-11-10 21:18:46 -0300

    create product/views.py and create template/list.html

--- loja_virtual/urls.py
+++ loja_virtual/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import home_page, about_page, contact_page, login_page, register_page
+from products.views import ProductListView, product_list_view
 
 urlpatterns = [
     path('', home_page),
@@ -11,6 +12,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_page),
     path('register/', register_page),
+    path('products/', ProductListView.as_view()),
+    path('products-fbv/', product_list_view),
+
 ]
 
 if settings.DEBUG:
