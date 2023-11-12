03f3d9e680e8f39b1b819b7aaf073b4246235a68
Henrique <henrique.coolcat15@gmail.com>
2023-11-12 14:53:05 -0300

    update products/views.py

--- products/views.py
+++ products/views.py
@@ -20,6 +20,7 @@ def product_list_view(request):
     }
     return render(request, "products/list.html", context)
 
+
 # Classe Detalhes
 class ProductDetailView(DetailView):
     #traz todos os produtos do banco de dados sem filtrar nada
@@ -28,12 +29,12 @@ class ProductDetailView(DetailView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-        print(args)
         return context
+
 
 # Function Detail
 def product_detail_view(request, pk=None, *args, **kwargs):
-    #instance = Product.objects.get(pk=pk)
+
     instance = get_object_or_404(Product, pk=pk)
     context = {
         'object': instance,
